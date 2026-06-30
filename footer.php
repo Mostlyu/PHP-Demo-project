f98b21b87b34e73c876e943875630c397f1e0b4d
Mostlyu <roymosl3@gmail.com>
2026-06-30 11:29:20 +0200

    clean up HTML structure in header.php and footer.php

--- footer.php
+++ footer.php
@@ -1,10 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <body>
-        <footer>
-            <p>&copy; 2026 TODO App. All rights reserved.</p>
-        </footer>
-    </body>
-</html>
+
+    <footer>
+        <p>&copy; 2026 TODO App. All rights reserved.</p>
+    </footer>
+
--- header.php
+++ header.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>TODO App</title>
-    </head>
-        <body>
-            <header>
-                <h1>TODO App</h1>
-            </header>
-        </body>
-</html>
+
+</head>
+    <body>
+        <header>
+            <h1>TODO App</h1>
+        </header>
+    </body>
+</head>
